bf2775abd8a01fbffb5705379008d4c98c9f8162
kachmazov <kachmazov@yandex-team.ru>
2021-12-04 13:17:16 +0400

    Setup

--- src/navigation/CoreNavigator.tsx
+++ src/navigation/CoreNavigator.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+// import Icon from 'react-native-vector-icons/FontAwesome5';
 import * as screens from '@/screens';
 import {CoreTabsParamList, CORE_ROUTES} from './types';
 
 const Tab = createBottomTabNavigator<CoreTabsParamList>();
 
-const icons = ['home', 'dice-four', 'user-circle'].map(
-  name =>
-    ({color, size}: {focused: boolean; color: string; size: number}) =>
-      <Icon name={name} color={color} size={size} solid />,
-);
+// const icons = ['home', 'dice-four', 'user-circle'].map(
+//   name =>
+//     ({color, size}: {focused: boolean; color: string; size: number}) =>
+//       <Icon name={name} color={color} size={size} solid />,
+// );
 
 export const CoreNavigator = () => (
   <Tab.Navigator>
     <Tab.Screen
       name={CORE_ROUTES.MAIN}
       component={screens.MainScreen}
-      options={{tabBarIcon: icons[0]}}
+      // options={{tabBarIcon: icons[0]}}
     />
     {/* <Tab.Screen
       name={CORE_ROUTES.SERVICES}
